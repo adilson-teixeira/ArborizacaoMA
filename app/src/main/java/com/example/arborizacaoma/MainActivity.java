a21340773464347c2804d7b49564aed26f66ed04
Adilson Lima <adilson.monte.alto@gmail.com>
2022-09-08 20:39:57 -0300

    Test Version

---
+++ app/src/main/java/com/example/arborizacaoma/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.arborizacaoma;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl("https://prototipo-pi3.herokuapp.com/");
+    }
+}
